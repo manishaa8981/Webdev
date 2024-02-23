d9f35f88e0875ed621224a8fe16a0e6b08f48908
manisha <manishaa8981@gmail.com>
2024-02-23 16:17:44 +0545

    added login and signup page

---
+++ webDev/src/Homepage/homenavbar.tsx
@@ -0,0 +1,40 @@
+import {NavLink} from "react-router-dom";
+import {Search, ShoppingBag} from "lucide-react";
+import {useState} from "react";
+import "./homenavbar.css"
+const HomeNavbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <>
+            <nav>
+                <a href={"/"} className={"title"}>The Candle Library</a>
+
+                <div className={"menu"} onClick={() => {
+                    setMenuOpen(!menuOpen);
+                }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="box">
+                    <i className="fa fa-search" aria-hidden="true"><Search/></i>
+                    <input type="search" placeholder="Search"/>
+                </div>
+                <ul className={menuOpen ? "open" : ""}>
+                    <li><NavLink to={"/Cart"}> <ShoppingBag className={"cart"} size={"30px"}/></NavLink></li>
+
+                    <li><NavLink to={"/login"}>
+                        <button className={"login-button"}>Sign In</button>
+                    </NavLink></li>
+
+                    <li><NavLink to={"/signup"}>
+                        <button className={"login-button"}>Sign Up</button>
+                    </NavLink></li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+
+export default HomeNavbar;
